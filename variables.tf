65541bd77d5e454418f6eeb16a4550df85da8291
DCamma <davide.cammarata@tamedia.ch>
2025-10-31 13:56:44 +0100

    fix: default bot config null

--- variables.tf
+++ variables.tf
@@ -28,11 +28,9 @@ variable "waf_logs_retention" {
 
 variable "google_whitelist_config" {
   description = "Configuration for whitelisting Googlebot IPs. Set 'whitelist' to false to disable the whitelisting. Doc https://developers.google.com/search/apis/ipranges/googlebot.json. The IPs are automatically parsed from the given url. Use 'insert_header' to add custom headers to these requests (the headers will be prefixed automatically with `x-amzn-waf-`)."
-  default = {
-    enable = false
-  }
+  default     = null
   type = object({
-    enable        = bool
+    enable        = optional(bool, false)
     url           = optional(string, "https://developers.google.com/search/apis/ipranges/googlebot.json")
     insert_header = optional(map(string), null)
   })
@@ -40,11 +38,9 @@ variable "google_whitelist_config" {
 
 variable "parsely_whitelist_config" {
   description = "Configuration for whitelisting Parse.ly crawler IPs. Set 'whitelist' to false to disable the whitelisting. The IPs are automatically parsed from the given url. Use 'insert_header' to add custom headers to these requests (the headers will be prefixed automatically with `x-amzn-waf-`)."
-  default = {
-    enable = false
-  }
+  default     = null
   type = object({
-    enable        = bool
+    enable        = optional(bool, false)
     url           = optional(string, "https://www.parse.ly/static/data/crawler-ips.json")
     insert_header = optional(map(string), null)
   })
@@ -52,11 +48,9 @@ variable "parsely_whitelist_config" {
 
 variable "k6_whitelist_config" {
   description = "Configuration for whitelisting the K6 load generators IPs. Set 'whitelist' to false to disable the whitelisting. Doc https://k6.io/docs/cloud/cloud-reference/cloud-ips/. The IPs are automatically parsed from the given url. Use 'insert_header' to add custom headers to these requests (the headers will be prefixed automatically with `x-amzn-waf-`)."
-  default = {
-    enable = false
-  }
+  default     = null
   type = object({
-    enable        = bool
+    enable        = optional(bool, false)
     url           = optional(string, "https://ip-ranges.amazonaws.com/ip-ranges.json")
     insert_header = optional(map(string), null)
   })
